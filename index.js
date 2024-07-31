01decb79d6d70855ff5d76f639841867f32a1454
hlelolwenkosi <1071hf@gmail.com>
2024-07-31 12:34:10 +0200

    Added css link and body properties under html

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
 let firstCard = 6
 let secondCard = 9
-
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message= ""
 
-console.log(sum)
+if (sum <= 20) {
+    message ="Do you want to draw a new card?🙂 "
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
+
+console.log(message)
