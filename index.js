96ff3e290b0c0920f9097d854e08e24745f76167
hlelolwenkosi <1071hf@gmail.com>
2024-08-01 09:34:21 +0200

    Added card display in html and javascript

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 let firstCard = 10
-let secondCard = 9
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message= ""
-
-
 let messageEl = document.getElementById("message-el")
-console.log(messageEl)
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent ="Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
