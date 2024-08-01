d891fbe090fea51f24140d861507a696a22dc236
hlelolwenkosi <1071hf@gmail.com>
2024-08-01 14:29:50 +0200

    Assigned values in the startGame

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-
+console.log(cards)
 
 function getRandomCard() {
     let randomNumer = Math.floor( Math.random()*13 ) + 1
@@ -26,6 +26,11 @@ function getRandomCard() {
 
 
 function startGame () {
+    isAlive=true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
